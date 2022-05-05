b735da3cd6987de8990d8ec69bc38e4997462c71
Cássia ☆ <102737875+cassiaplima@users.noreply.github.com>
2022-05-05 10:51:28 -0300

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,30 @@
+//Exercicios-2 -Cassia Pereira Lima
+
+//Atividade-1
+
+function name(a) {
+    console.log(a);
+    return
+}
+ name('cassia')
+
+ //atividade-2
+
+ function soma(a,b){
+     return console.log(a+b)
+ }
+ soma(3,5)
+
+ //atividade-3
+ function multiplicacao (c,e){
+     console.log(c*e)
+     return
+}
+multiplicacao(4,6)
+
+//atividade-4
+function media(a,b,c,e){
+    let res = (a+b+c+e)/a
+    return console.log(res)
+}
+media(3,4,5,6)
